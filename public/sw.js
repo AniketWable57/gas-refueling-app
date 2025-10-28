9bdbefbe473b2d93127422609bcb820bcca761c6
aniketwable <aniketwable57@gmail.com>
2025-10-28 17:08:05 +0530

    errorsolve

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,4 @@
+// public/sw.js
 const CACHE_NAME = 'gas-mediator-v1';
 const urlsToCache = [
   '/',
@@ -24,4 +25,11 @@ self.addEventListener('fetch', (event) => {
       }
     )
   );
+});
+
+// Skip waiting message for updates
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
